f57e56ab8be912f8e1170c37f311dc4ac69fd97e
Gopal Saini <gopal.saini.work@gmail.com>
2025-04-06 14:46:42 -0400

    Implement Server-Sent Events (SSE) with Flask and add index.html template

--- server.py
+++ server.py
@@ -1,20 +1,25 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
 import json
 import time
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/events')
 def sse():
-    def event_streams():
+    def event_stream():
         while True:
             data = {
-                'message': 'Hello from python server',
-                'time': time.strftime('%Y-%m-%d %H:%M:%S')
+                "message": "Hello from Python server",
+                "timestamp": time.strftime('%Y-%m-%d %H:%M:%S')
             }
             yield f"data: {json.dumps(data)}\n\n"
             time.sleep(5)
-    return Response(event_streams(), content_type='text/event-stream')
 
-if __name__ == 'main':
-    app.run(port=5000, debug=True, threaded=True)
+    return Response(event_stream(), content_type='text/event-stream')
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, port=5000)
